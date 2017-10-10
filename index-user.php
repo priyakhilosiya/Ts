3de6fb528ddcb9bfe50af783e04942b08459e423
priyakhilosiya <pkhilosiya12@gmail.com>
2017-10-09 19:56:38 -0700

    Add files via upload

---
+++ index-user.php
@@ -0,0 +1,131 @@
+<div class="container-fluid">
+        <div class="page-title">
+            <h1 class="title"><i class="ico-users"></i>
+Attendees
+</h1>
+        </div>
+                <!--  header -->
+        <div class="page-header page-header-block row">
+            <div class="row">
+
+<div class="col-md-9">
+    <div class="btn-toolbar" role="toolbar">
+        <div class="btn-group btn-group-responsive">
+            <button data-toggle="modal"   href="javascript:void(0);"  data-href="<?=admin_path()?>users/addattendee"  class="loadModal btn btn-success" type="button"><i class="ico-user-plus"></i> Invite Attendee</button>
+        </div>
+
+        <div class="btn-group btn-group-responsive">
+            <button data-toggle="modal"   href="javascript:void(0);"  data-href="<?=admin_path()?>users/inviteAttendees" class="loadModal btn btn-success" type="button"><i class="ico-file"></i> Invite Attendees</button>
+        </div>
+
+        <div class="btn-group btn-group-responsive">
+            <a class="btn btn-success" href="http://nirav-event.kwetoo.com/event/2/attendees/print" target="_blank"><i class="ico-print"></i> Print Attendee List</a>
+        </div>
+        <div class="btn-group btn-group-responsive">
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+                <i class="ico-users"></i> Export <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="http://nirav-event.kwetoo.com/event/2/attendees/export/xlsx">Excel (XLSX)</a></li>
+                <li><a href="http://nirav-event.kwetoo.com/event/2/attendees/export/xls">Excel (XLS)</a></li>
+                <li><a href="http://nirav-event.kwetoo.com/event/2/attendees/export/csv">CSV</a></li>
+                <li><a href="http://nirav-event.kwetoo.com/event/2/attendees/export/html">HTML</a></li>
+            </ul>
+        </div>
+        <div class="btn-group btn-group-responsive">
+            <button data-toggle="modal"   href="javascript:void(0);"  data-href="<?=admin_path()?>users/messageAll" class="loadModal btn btn-success" type="button"><i class="ico-envelope"></i> Message</button>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+   <form method="GET" action="" accept-charset="UTF-8">
+    <div class="input-group">
+        <input name="q" value="" placeholder="Search Attendees.." class="form-control" type="text">
+        <span class="input-group-btn">
+            <button class="btn btn-default" type="submit"><i class="ico-search"></i></button>
+        </span>
+    </div>
+   </form>
+</div>
+            </div>
+        </div>
+        <!--/  header -->
+
+        <!--Content-->
+
+<!--Start Attendees table-->
+<div class="row">
+    <div class="col-md-12">
+                <div class="panel">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>
+                               <a href="?sort_by=first_name&amp;sort_order=asc&amp;q=&amp;page=1" class="col-sort ">Name<i class="ico-arrow-up22"></i></a>
+                            </th>
+                            <th>
+                               <a href="?sort_by=email&amp;sort_order=asc&amp;q=&amp;page=1" class="col-sort ">Email<i class="ico-arrow-up22"></i></a>
+                            </th>
+                            <th>
+                               <a href="?sort_by=ticket_id&amp;sort_order=asc&amp;q=&amp;page=1" class="col-sort ">Ticket<i class="ico-arrow-up22"></i></a>
+                            </th>
+                            <th>
+                               <a href="?sort_by=order_reference&amp;sort_order=asc&amp;q=&amp;page=1" class="col-sort ">Order Ref.<i class="ico-arrow-up22"></i></a>
+                            </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php if($allUserDetails!="0" && count($allUserDetails)>0){  ?>
+
+                        <?php foreach ($allUserDetails as $key=>$val){?>
+                    <tr class="attendee_5 ">
+                            <td> <?php echo $val['U_FNAME']." ".$val['U_LNAME'];?></td>
+                            <td>
+                                <a data-modal-id="MessageAttendee" class="loadModal" href="javascript:void(0);"  data-href="<?=admin_path()?>users/messageAttendee"> <?php echo $val['U_EMAIL'];?></a>
+                            </td>
+                            <td>
+                                <?php echo $val['ORD_T_NAME'];?>
+                            </td>
+                            <td>
+                                <a href="javascript:void(0);"  data-href="<?=admin_path()?>users/orderView" title="View Order '<?=$val['ORD_REFERENCE'];?>'" class="loadModal">
+                                     <?php echo $val['ORD_REFERENCE'];?>
+                                </a>
+                            </td>
+                            <td class="text-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+                                    <ul class="dropdown-menu">
+                                                                                <li><a href="javascript:void(0);"  data-modal-id="MessageAttendee" data-href="<?=admin_path()?>users/messageAttendee/<?=$val['ORD_ID'];?>/<?=$val['U_ID'];?>" class="loadModal"> Message</a></li>
+                                                                                <li><a href="javascript:void(0);"  data-href="<?=admin_path()?>users/resendTicket/<?=$val['ORD_ID'];?>/<?=$val['U_ID'];?>" class="loadModal"> Resend Ticket</a></li>
+                                        <li><a href="<?=admin_path()?>users/downloadPdf/<?=$val['ORD_ID'];?>/<?=$val['U_ID'];?>">Download PDF Ticket</a></li>
+                                    </ul>
+                                </div>
+
+                                <a href="javascript:void(0);"  data-href="<?=admin_path()?>users/editAttendee/<?=$val['ORD_ID'];?>/<?=$val['U_ID'];?>" class="loadModal btn btn-xs btn-primary"> Edit</a>
+
+                                <a href="javascript:void(0);"  data-href="<?=admin_path()?>users/cancelAttendee/<?=$val['ORD_ID'];?>/<?=$val['U_ID'];?>" class="loadModal btn btn-xs btn-danger"> Cancel</a>
+                            </td>
+                      </tr>
+                      <?php }?>
+                      <?php }else{?>
+
+                      <tr><td>no recorrd</td></tr>
+
+                      <?php }?>
+
+
+                   </tbody>
+
+                </table>
+            </div>
+        </div>
+            </div>
+    <div class="col-md-12">
+
+    </div>
+</div>    <!--/End attendees table-->
+
+        <!--/Content-->
+    </div>
